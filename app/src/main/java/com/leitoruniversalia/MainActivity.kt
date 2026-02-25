eb4e33ad4262cc7647cde4c7f405122fc90f89eb
wallaceleandro <wallaceleandro8.com>
2026-02-25 06:07:41 -0300

    Adicionando layout com campo e botão

--- app/src/main/java/com/leitoruniversalia/MainActivity.kt
+++ app/src/main/java/com/leitoruniversalia/MainActivity.kt
@@ -2,15 +2,11 @@ package com.leitoruniversalia
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val textView = TextView(this)
-        textView.text = "Leitor Universal IA funcionando 🚀"
-        setContentView(textView)
+        setContentView(R.layout.activity_main)
     }
 }
