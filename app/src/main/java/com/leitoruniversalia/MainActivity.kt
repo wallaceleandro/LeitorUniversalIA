a8097583bb91470b5f1bd30bf6d583b6f0c517cf
wallaceleandro <wallaceleandro8.com>
2026-03-01 17:04:56 -0300

    Layout principal corrigido e validado

--- app/src/main/java/com/leitoruniversalia/MainActivity.kt
+++ app/src/main/java/com/leitoruniversalia/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.leitoruniversalia
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.leitoruniversalia.leitor.LeitorActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnLeitor = findViewById<Button>(R.id.btnLeitor)
+
+        btnLeitor.setOnClickListener {
+            val intent = Intent(this, LeitorActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
